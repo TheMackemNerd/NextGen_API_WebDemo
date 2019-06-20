ed58a835fe80cb1af5751c22ea727c396efe9ef1
John Ferguson <John.Ferguson@IRIS.CO.UK>
2019-06-20 10:29:53 +0100

    fix cors

--- js/securecheck.js
+++ js/securecheck.js
@@ -6,33 +6,6 @@ function setUserName() {
 
 }
 
-function getUserRecord() {
-
-    var request = new XMLHttpRequest();
-
-    console.log("Preparing to call API");
-    request.setRequestHeader("Authorization", session.getItem("token"));
-    request.open('GET', 'https://2y3ps0tqaj.execute-api.eu-west-1.amazonaws.com/poc/users?sub=' + sessionStorage.getItem("userKey"), true);
-    request.onload = function () {
-
-        if (request.status != 200) {
-            console.log("The API returned an error");
-            var err = JSON.parse(this.response).description;
-            window.location.replace("error.html?errordesc=" + encodeURI(err));
-        }
-        else {
-            console.log("API call Success");
-            var data = this.response;
-            sessionStorage.setItem("user", JSON.stringify(data));
-            window.location.replace("index.html");
-        }
-
-    }
-
-    request.send();
-
-}
-
 function isSecure() {
     if (!secureCheck()) {
         window.location.replace("https://hcm-hub-rnd.auth.eu-west-1.amazoncognito.com/login?response_type=token&client_id=57vo0lcv2gq0822td26v9nhnh6&redirect_uri=https://ec2-34-241-195-116.eu-west-1.compute.amazonaws.com/callback.html");
@@ -109,33 +82,33 @@ function getToken() {
 
 }
 
-    function parseJwt(token) {
-        try {
+function parseJwt(token) {
+    try {
 
-            console.log("Parsing Token");
-            const base64HeaderUrl = token.split('.')[0];
-            const base64Header = base64HeaderUrl.replace('-', '+').replace('_', '/');
-            const headerData = JSON.parse(window.atob(base64Header));
+        console.log("Parsing Token");
+        const base64HeaderUrl = token.split('.')[0];
+        const base64Header = base64HeaderUrl.replace('-', '+').replace('_', '/');
+        const headerData = JSON.parse(window.atob(base64Header));
 
-            const base64Url = token.split('.')[1];
-            const base64 = base64Url.replace('-', '+').replace('_', '/');
-            tokenData = JSON.parse(window.atob(base64));
-            tokenData.header = headerData;
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace('-', '+').replace('_', '/');
+        tokenData = JSON.parse(window.atob(base64));
+        tokenData.header = headerData;
 
-            // Checking if the Token has expired
-            if (isTokenValid()) {
-                console.log("Token is valid");
-                return true;
-            }
-            else {
-                console.log("Token is invalid");
-                return false;
-            }
-
-        } catch (err) {
+        // Checking if the Token has expired
+        if (isTokenValid()) {
+            console.log("Token is valid");
+            return true;
+        }
+        else {
+            console.log("Token is invalid");
             return false;
         }
+
+    } catch (err) {
+        return false;
     }
+}
 
 function isTokenValid() {
 
@@ -144,4 +117,39 @@ function isTokenValid() {
         console.log("Checking if Token is Valid");
         return !(tokenData.exp < new Date().getTime() / 1000);    
 
+}
+
+function getUserRecord() {
+
+    var request = new XMLHttpRequest();
+
+    var sub = sessionStorage.getItem("userKey");
+
+    if (sub === null) {
+        sub = JSON.parse(getToken()).sub;
     }
+
+    console.log("Preparing to call API");
+    request.open('GET', 'https://2y3ps0tqaj.execute-api.eu-west-1.amazonaws.com/poc/users?sub=' + sub, true);
+    request.setRequestHeader("Authorization", sessionStorage.getItem("token"));
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Access-Control-Allow-Origin", "https://ec2-34-241-195-116.eu-west-1.compute.amazonaws.com");
+    request.onload = function () {
+
+        if (request.status != 200) {
+            console.log("The API returned an error");
+            var err = JSON.parse(this.response).description;
+            window.location.replace("error.html?errordesc=" + encodeURI(err));
+        }
+        else {
+            console.log("API call Success");
+            var data = this.response;
+            sessionStorage.setItem("user", JSON.stringify(data));
+            window.location.replace("index.html");
+        }
+
+    }
+
+    request.send();
+
+}
