c3ab7745bf04d14d3bf13e7695d4d7983dfd3d4f
John Ferguson <John.Ferguson@IRIS.CO.UK>
2019-06-24 10:31:50 +0100

    user invite functions

--- invite.js
+++ invite.js
@@ -1,7 +1,15 @@
 function doInvite() {
 
-    var user = sessionStorage.getItem("user");
-    var tenant = JSON.parse(JSON.parse(user)).tenant;
+    var token = getCookie("accesstoken");
+
+    if (getCookie("user") != null) {
+        var tenant = JSON.parse(getCookie("user")).tenant;
+        document.getElementById("tenant").innerHTML = "Tenant: " + tenant;
+    }
+    else {
+        console.log("No user cookie can be found so we cannot get the tenancy");
+        window.location.replace("error.html?errordesc=" + encodeURI("Cannot determine your Tenant ID"));
+    }
 
     var urlParams = new URLSearchParams(window.location.search);
     var emailaddress = urlParams.get('emailaddress');
@@ -12,16 +20,16 @@ function doInvite() {
     var request = new XMLHttpRequest();
 
     request.open('POST', 'https://2y3ps0tqaj.execute-api.eu-west-1.amazonaws.com/poc/users');
-    console.log("Authorization: " + sessionStorage.getItem("encodedtoken"));
-    request.setRequestHeader("Authorization", sessionStorage.getItem("encodedtoken"));
+    
+    request.setRequestHeader("Authorization", token);
     request.setRequestHeader("Content-Type", "application/json");
     request.setRequestHeader("Access-Control-Allow-Origin", "https://ec2-34-241-195-116.eu-west-1.compute.amazonaws.com");
 
     request.onload = function () {
 
         if (request.status == 200 || request.status == 201) {
-            console.log("API call Success");
-
+            console.log("API call Success: " + JSON.stringify(request.data));
+            
         }
         else {
             console.log("The API returned an error");
