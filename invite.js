44368913464312f14011e7dee2ca45ea34cc0c89
John Ferguson <John.Ferguson@IRIS.CO.UK>
2019-06-24 10:38:53 +0100

    js files not updating properly

--- invite.js
+++ invite.js
@@ -1,8 +1,13 @@
 function doInvite() {
 
+    console.log("Entering doInvite");
+
+    console.log("Retrieving Access Token");
     var token = getCookie("accesstoken");
 
+    console.log("Retrieving User");
     if (getCookie("user") != null) {
+        console.log("User Cookie retrieved");
         var tenant = JSON.parse(getCookie("user")).tenant;
         document.getElementById("tenant").innerHTML = "Tenant: " + tenant;
     }
@@ -11,21 +16,23 @@ function doInvite() {
         window.location.replace("error.html?errordesc=" + encodeURI("Cannot determine your Tenant ID"));
     }
 
+    console.log("Getting URL Parameters");
     var urlParams = new URLSearchParams(window.location.search);
     var emailaddress = urlParams.get('emailaddress');
     var fullname = urlParams.get('fullname');
 
     var data = "{ 'email': " + emailaddress + "', 'name' : '" + fullname + "', 'tenant':'" + tenant + "'}";
 
+    console.log("Creating API Call");
     var request = new XMLHttpRequest();
-
-    request.open('POST', 'https://2y3ps0tqaj.execute-api.eu-west-1.amazonaws.com/poc/users');
-    
+    request.open('POST', 'https://2y3ps0tqaj.execute-api.eu-west-1.amazonaws.com/poc/users');    
     request.setRequestHeader("Authorization", token);
     request.setRequestHeader("Content-Type", "application/json");
     request.setRequestHeader("Access-Control-Allow-Origin", "https://ec2-34-241-195-116.eu-west-1.compute.amazonaws.com");
 
     request.onload = function () {
+
+        console.log("Performing API Callback");
 
         if (request.status == 200 || request.status == 201) {
             console.log("API call Success: " + JSON.stringify(request.data));
@@ -40,6 +47,7 @@ function doInvite() {
 
     }
 
+    console.log("Calling API");
     request.send(data);
 
 }
--- js/invite.js
+++ js/invite.js
@@ -1,36 +1,53 @@
 function doInvite() {
 
-    var user = sessionStorage.getItem("user");
-    var tenant = JSON.parse(JSON.parse(user)).tenant;
+    console.log("Entering doInvite");
 
+    console.log("Retrieving Access Token");
+    var token = getCookie("accesstoken");
+
+    console.log("Retrieving User");
+    if (getCookie("user") != null) {
+        console.log("User Cookie retrieved");
+        var tenant = JSON.parse(getCookie("user")).tenant;
+        document.getElementById("tenant").innerHTML = "Tenant: " + tenant;
+    }
+    else {
+        console.log("No user cookie can be found so we cannot get the tenancy");
+        window.location.replace("error.html?errordesc=" + encodeURI("Cannot determine your Tenant ID"));
+    }
+
+    console.log("Getting URL Parameters");
     var urlParams = new URLSearchParams(window.location.search);
     var emailaddress = urlParams.get('emailaddress');
     var fullname = urlParams.get('fullname');
 
     var data = "{ 'email': " + emailaddress + "', 'name' : '" + fullname + "', 'tenant':'" + tenant + "'}";
 
+    console.log("Creating API Call");
     var request = new XMLHttpRequest();
-
-    request.open('POST', 'https://2y3ps0tqaj.execute-api.eu-west-1.amazonaws.com/poc/users');
-    console.log("Authorization: " + sessionStorage.getItem("encodedtoken"));
-    request.setRequestHeader("Authorization", sessionStorage.getItem("encodedtoken"));
+    request.open('POST', 'https://2y3ps0tqaj.execute-api.eu-west-1.amazonaws.com/poc/users');    
+    request.setRequestHeader("Authorization", token);
     request.setRequestHeader("Content-Type", "application/json");
     request.setRequestHeader("Access-Control-Allow-Origin", "https://ec2-34-241-195-116.eu-west-1.compute.amazonaws.com");
 
     request.onload = function () {
 
-        if (request.status != 200) {
+        console.log("Performing API Callback");
+
+        if (request.status == 200 || request.status == 201) {
+            console.log("API call Success: " + JSON.stringify(request.data));
+            
+        }
+        else {
             console.log("The API returned an error");
             var err = JSON.parse(this.response).message;
             console.log(err);
             window.location.replace("error.html?errordesc=" + encodeURI(err));
         }
-        else {
-            console.log("API call Success");            
-        }
 
     }
 
+    console.log("Calling API");
     request.send(data);
 
 }
