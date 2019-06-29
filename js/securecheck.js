b08109d6ef7c48a9beb32fa1242b7edaa172765f
John Ferguson <John.Ferguson@IRIS.CO.UK>
2019-06-29 21:10:38 +0100

    change lookup to /me

--- js/securecheck.js
+++ js/securecheck.js
@@ -406,8 +406,8 @@ function getUserRecord(token, sub, callback) {
 
         var request = new XMLHttpRequest();
         console.log("Preparing to call API");
-        console.log("Sub: " + sub);
-        request.open('GET', apiGateway + '/poc/users?sub=' + sub, true);
+        //request.open('GET', apiGateway + '/poc/users?sub=' + sub, true);
+        request.open('GET', apiGateway + '/poc/users/me', true);
         request.setRequestHeader("Authorization", token);
         request.setRequestHeader("x-api-key", apiKey);
         request.setRequestHeader("Content-Type", "application/json");
