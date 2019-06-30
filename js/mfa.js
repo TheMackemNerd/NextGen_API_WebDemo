edf34bbaf14bdf9851ac8248dbd06cf003cc8dbc
John Ferguson <John.Ferguson@IRIS.CO.UK>
2019-06-30 22:35:35 +0100

    mfa

---
+++ js/mfa.js
@@ -0,0 +1,28 @@
+function getMFAStatus() {
+
+    var request = new XMLHttpRequest();
+    console.log("Query Tenant: " + queryTenant);
+    request.open('GET', 'https://2y3ps0tqaj.execute-api.eu-west-1.amazonaws.com/poc/users/mfa', true);
+    request.setRequestHeader("Authorization", token);
+    request.setRequestHeader("x-api-key", apiKey);
+    request.onload = function () {
+
+        if (request.status != 200) {
+            console.log("The API returned an error");
+            var err = JSON.parse(this.response).message;
+            console.log(err);
+            window.location.replace("error.html?errordesc=" + encodeURI(err));
+        }
+        else {
+            console.log("API call Success");
+            var data = this.response;
+            document.getElementById("mfaStatus").innerHTML = JSON.parse(data).mfa_enabled;
+
+        }
+
+    }
+
+    request.send();
+
+
+}
